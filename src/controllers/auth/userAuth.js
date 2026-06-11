dc467aa1120a34e0b86036195fcc69f416e6eae0
lounghminghsakal <lounghminghsakal@flaerhomes.com>
2026-06-11 07:54:42 +0530

    Added user auth context and api for it

--- src/controllers/auth/userAuth.js
+++ src/controllers/auth/userAuth.js
@@ -6,22 +6,22 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const createUser = async (req, res) => {
-  try{
+  try {
     validateCreateUserPayload(req.body);
 
     const newUserPayload = pickAllowedFields(createUserAllowedFields, req.body);
     const hashedPassword = await bcrypt.hash(newUserPayload.user_password, 10);
-    const newUser = await User.create({...newUserPayload, user_password: hashedPassword});
+    const newUser = await User.create({ ...newUserPayload, user_password: hashedPassword });
     res.json({
       status: "success",
       message: "User created successfully",
       data: newUser
     });
 
-  } catch(err) {
+  } catch (err) {
     res.status(400).json({
       status: "failure",
-      message: "Failed to create user: "+err.message
+      message: "Failed to create user: " + err.message
     });
   }
 }
@@ -32,15 +32,15 @@ export const userLogin = async (req, res) => {
     const user = await validateUserLoginPayload(req.body);
     const { user_email, user_password } = req.body;
     const isPasswordValid = await bcrypt.compare(user_password, user.user_password);
-    if(!isPasswordValid) {
+    if (!isPasswordValid) {
       return res.status(400).json({
         status: "failure",
         message: "Invalid credentials"
       });
     }
 
-    const userToken = await jwt.sign({userId: user?._id}, process.env.JWT_SECRET, {expiresIn: "1d"});
-    res.cookie("userToken", userToken);    
+    const userToken = await jwt.sign({ userId: user?._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    res.cookie("userToken", userToken);
 
     res.json({
       status: "success",
@@ -49,23 +49,56 @@ export const userLogin = async (req, res) => {
     });
 
 
-  } catch(err) {
+  } catch (err) {
     res.status(400).json({
       status: "failure",
-      message: "Failed to login user: "+err.message
+      message: "Failed to login user: " + err.message
+    });
+  }
+}
+
+export const getLoggedInUser = async (req, res) => {
+  try {
+    const userToken = req.cookies.token;
+    if (!userToken) {
+      return res.status(404).json({
+        status: "failure",
+        message: "User token not found",
+      });
+    }
+    const decodedMessage = await jwt.verify(userToken, process.env.JWT_SECRET);
+    const { userId } = decodedMessage;
+    const loggedInUser = await User.findById(userId);
+    if (!loggedInUser) {
+      return res.status(404).json({
+        status: "failure",
+        message: "User not found"
+      });
+    }
+
+    res.json({
+      status: "success",
+      message: "Logged in user data fetched successfully",
+      data: loggedInUser
+    });
+
+  } catch (err) {
+    res.status(400).json({
+      status: "failure",
+      message: "Failed to fetch logged in user data "+err.message
     });
   }
 }
 
 
 export const userLogout = (req, res) => {
-  try{
+  try {
     res.clearCookie("userToken");
     res.json({
       status: "success",
       message: "user logged out successfully"
     });
-  } catch(err) {
+  } catch (err) {
 
   }
 }
--- src/routes/auth_routes/userAuthRoutes.js
+++ src/routes/auth_routes/userAuthRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createUser, userLogin, userLogout } from "../../controllers/auth/userAuth.js";
+import { createUser, userLogin, userLogout, getLoggedInUser } from "../../controllers/auth/userAuth.js";
 
 const userAuthRouter = express.Router();
 
 userAuthRouter.post("/signup", createUser);
 
 userAuthRouter.post("/login", userLogin);
+
+userAuthRouter.get("/me", getLoggedInUser);
 
 userAuthRouter.post("/logout", userLogout);
 
